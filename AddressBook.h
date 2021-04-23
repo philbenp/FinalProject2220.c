78ecc9c1584b0a9fbf12b9560c9834bfd8c278fa
philbenp <82547190+philbenp@users.noreply.github.com>
2021-04-23 08:36:16 -0400

    Update AddressBook.h

--- AddressBook.h
+++ AddressBook.h
@@ -1,18 +1,26 @@
+#ifndef PERSON_H
+#define PERSON_H
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #define DIM 10
 
-struct person {
+typedef struct Person_struct {
 	char name[40];
 	char surname[40];
-	char date[11];
-	char number[11];
-    char address[100];
-};
+	char dateOfBirth[11];
+	char phoneNumber[11];
+	char emailAddress[100];
+	char address[100];
+} Person;
 
-typedef struct person Person;
+void dataEntry(Person *);
+void createContact();
+void retrieveContact();
+void updateContact();
+void deleteContact();
+void printPerson(Person);
+void printAddressBook(Person *);
 
-void dataEntry(Person* person);
-void printPerson(Person person);
-void printAddressBook(Person* addbook[10]);
+#endif
